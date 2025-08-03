7f80761a2f351ab484685a91f697f254d7298307
Davide Nale <davide.nale.99@gmail.com>
2025-08-03 23:24:09 +0200

    feat: swapchain creation

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -1,47 +1,161 @@
+use std::sync::Arc;
+
+use vulkano::VulkanLibrary;
+use vulkano::device::physical::PhysicalDeviceType;
+use vulkano::device::{
+    Device, DeviceCreateInfo, DeviceExtensions, Queue, QueueCreateInfo, QueueFlags,
+};
+use vulkano::image::ImageUsage;
+use vulkano::instance::{Instance, InstanceCreateFlags, InstanceCreateInfo};
+use vulkano::swapchain::{PresentMode, Surface, Swapchain, SwapchainCreateInfo};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::window::{Window, WindowId};
 
-use anyhow::Context;
-
-#[derive(Default)]
 pub struct Engine {
-    window: Option<Window>,
+    instance: Arc<Instance>,
+    device: Arc<Device>,
+    queue: Arc<Queue>,
+    rcx: Option<RenderContext>,
+}
+
+pub struct RenderContext {
+    window: Arc<Window>,
+    swapchain: Arc<Swapchain>,
 }
 
 impl Engine {
-    pub fn new() -> anyhow::Result<Self> {
-        Ok(Engine::default())
-    }
+    pub fn new(event_loop: &EventLoop<()>) -> Self {
+        let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+        let required_extensions = Surface::required_extensions(event_loop).unwrap();
 
-    pub fn run(mut self) {
-        let event_loop = EventLoop::new()
-            .context("Failed to create event loop")
-            .unwrap();
-        event_loop.set_control_flow(ControlFlow::Poll);
+        let instance = Instance::new(
+            library,
+            InstanceCreateInfo {
+                enabled_extensions: required_extensions,
+                flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+                ..Default::default()
+            },
+        )
+        .unwrap();
 
-        event_loop.run_app(&mut self).unwrap();
+        let device_extensions = DeviceExtensions {
+            khr_swapchain: true,
+            ..DeviceExtensions::empty()
+        };
+
+        let (physical_device, queue_family_index) = instance
+            .enumerate_physical_devices()
+            .expect("Could not enumerate devices")
+            .filter(|p| p.supported_extensions().contains(&device_extensions))
+            .filter_map(|p| {
+                p.queue_family_properties()
+                    .iter()
+                    .enumerate()
+                    .position(|(i, q)| {
+                        q.queue_flags.intersects(QueueFlags::GRAPHICS)
+                            && p.presentation_support(i as u32, event_loop).unwrap()
+                    })
+                    .map(|i| (p, i as u32))
+            })
+            .min_by_key(|(p, _)| match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+                _ => 5,
+            })
+            .expect("No devices available");
+
+        println!(
+            "Using device: {} (type: {:?})",
+            physical_device.properties().device_name,
+            physical_device.properties().device_type,
+        );
+
+        let (device, mut queues) = Device::new(
+            physical_device,
+            DeviceCreateInfo {
+                enabled_extensions: device_extensions,
+                // here we pass the desired queue family to use by index
+                queue_create_infos: vec![QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )
+        .expect("failed to create device");
+
+        let queue = queues.next().unwrap();
+
+        Engine {
+            instance,
+            device,
+            queue,
+            rcx: None,
+        }
     }
 }
 
 impl ApplicationHandler for Engine {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(
+        let window = Arc::new(
             event_loop
-                .create_window(Window::default_attributes())
+                .create_window(Window::default_attributes().with_resizable(false))
                 .unwrap(),
         );
+        let surface = Surface::from_window(self.instance.clone(), window.clone()).unwrap();
+
+        let (swapchain, images) = {
+            let surface_capabilities = self
+                .device
+                .physical_device()
+                .surface_capabilities(&surface, Default::default())
+                .expect("failed to get surface capabilities");
+            let (image_format, _) = self
+                .device
+                .physical_device()
+                .surface_formats(&surface, Default::default())
+                .unwrap()[0];
+
+            let dimensions = window.clone().inner_size();
+
+            Swapchain::new(
+                self.device.clone(),
+                surface,
+                SwapchainCreateInfo {
+                    min_image_count: surface_capabilities.min_image_count.max(2),
+                    image_format,
+                    image_extent: [dimensions.width, dimensions.height],
+                    image_usage: ImageUsage::COLOR_ATTACHMENT,
+                    composite_alpha: surface_capabilities
+                        .supported_composite_alpha
+                        .into_iter()
+                        .next()
+                        .unwrap(),
+                    present_mode: PresentMode::Fifo,
+                    ..Default::default()
+                },
+            )
+            .expect("unable to create swapchain")
+        };
+
+        self.rcx = Some(RenderContext { window, swapchain })
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
+        let rcx = self.rcx.as_mut().unwrap();
+
         match event {
             WindowEvent::CloseRequested => {
                 println!("The close button was pressed; stopping");
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
-                self.window.as_ref().unwrap().request_redraw();
+                rcx.window.as_ref().request_redraw();
             }
             _ => (),
         }
